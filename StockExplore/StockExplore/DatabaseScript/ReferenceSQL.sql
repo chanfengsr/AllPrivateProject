07488b955d6e095d8732a92d2346be8bd32bcfb6
chanfengsr <chanfengsr@gmail.com>
2017-07-12 16:51:25 +0800

    2017-7-12

--- StockExplore/StockExplore/DatabaseScript/ReferenceSQL.sql
+++ StockExplore/StockExplore/DatabaseScript/ReferenceSQL.sql
@@ -1,12 +1,16 @@
 -- 统计当天收盘收在最低价，
-WITH RelateRecord (MarkType, StkCode, TradeDay, [Open], [High], [Low], [Close], Volume, Amount, RecId, PrevRecId, NextRecId) AS
+WITH RelateRecord (MarkType, StkCode, TradeDay, [Open], [High], [Low], [Close], Volume, Amount, RecId, PrevRecId, NextRecId, Next2RecId) AS
 (
     SELECT MarkType,StkCode,TradeDay,[Open],[High],[Low],[Close],Volume,Amount,RecId,
         PrevRecId = ISNULL((SELECT TOP 1 RecId FROM KLineDay WHERE MarkType = LowClose.MarkType AND StkCode = LowClose.StkCode AND TradeDay < LowClose.TradeDay ORDER BY TradeDay DESC), -1),
-        NextRecId = ISNULL((SELECT TOP 1 RecId FROM KLineDay WHERE MarkType = LowClose.MarkType AND StkCode = LowClose.StkCode AND TradeDay > LowClose.TradeDay ORDER BY TradeDay ASC), -1)
+        NextRecId = ISNULL((SELECT TOP 1 RecId FROM KLineDay WHERE MarkType = LowClose.MarkType AND StkCode = LowClose.StkCode AND TradeDay > LowClose.TradeDay ORDER BY TradeDay ASC), -1),
+        Next2RecId = ISNULL((SELECT TOP 1 a.RecId FROM (    SELECT TOP 2 RecId, TradeDay FROM KLineDay 
+                                                            WHERE MarkType = LowClose.MarkType AND StkCode = LowClose.StkCode AND TradeDay > LowClose.TradeDay 
+                                                            ORDER BY TradeDay ASC
+                                                      ) a ORDER BY a.TradeDay DESC), -1)
     FROM (
             SELECT MarkType,StkCode,TradeDay,[Open],[High],[Low],[Close],Volume,Amount,RecId 
-            FROM KLineDay WHERE TradeDay >= '2010/1/1' AND [Close] = [Low]    
+            FROM KLineDay WHERE TradeDay >= '2010/1/1' AND [Close] = [Low]    -- 最低价作收
         ) LowClose
 )
 SELECT COUNT(1)
@@ -15,9 +19,10 @@ JOIN   KLineDay prev
   ON   cur.PrevRecId = prev.RecId 
 JOIN   KLineDay nxt
   ON   cur.NextRecId = nxt.RecId
-WHERE  cur.[Close] > prev.[Close] * 0.901 -- 去掉跌停 
-AND    cur.[Close] < prev.[Close]
-AND    cur.[Close] < prev.[Close] * 0.98 -- 当日跌幅大于 2%
-
-AND    cur.[Close] > nxt.[Open]
---ORDER BY cur.TradeDay DESC
+JOIN   KLineDay nxt2
+  ON   cur.Next2RecId = nxt2.RecId
+WHERE  cur.[Close] > prev.[Close] * 0.901   -- 去掉跌停 
+--AND    cur.[Close] < prev.[Close]         -- 第二天真阳线
+AND    cur.[Close] < prev.[Close] * 0.98    -- 当日跌幅大于 2%
+AND    cur.[Close] > nxt.[Open]             -- 第二天低开
+AND    nxt.[Close] < nxt2.[Close]           -- 第三天阳线
---
+++ StockExplore/StockExplore/ReferenceCode.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace StockExplore
+{
+    internal class ReferenceCode
+    {
+        // 读取日线文件
+        public static void Main1(string[] args)
+        {
+            const int lineLength = 32;
+            using (var reader = new BinaryReader(File.OpenRead(@"r:\sh600583.day")))
+            {
+                for (int i = 0; i < 2; i++)
+                {
+                    int beg = i * lineLength;
+                    int offset = beg;
+
+                    // 下面的 Seek 都可以不要！
+                    reader.BaseStream.Seek(offset, SeekOrigin.Begin);
+                    string date = reader.ReadUInt32().ToString();
+
+                    offset += 4;
+                    reader.BaseStream.Seek(offset, SeekOrigin.Begin);
+                    string open = reader.ReadUInt32().ToString();
+
+                    offset += 4;
+                    reader.BaseStream.Seek(offset, SeekOrigin.Begin);
+                    string high = reader.ReadUInt32().ToString();
+
+                    offset += 4;
+                    reader.BaseStream.Seek(offset, SeekOrigin.Begin);
+                    string low = reader.ReadUInt32().ToString();
+
+                    offset += 4;
+                    reader.BaseStream.Seek(offset, SeekOrigin.Begin);
+                    string close = reader.ReadUInt32().ToString();
+
+                    offset += 4;
+                    reader.BaseStream.Seek(offset, SeekOrigin.Begin);
+                    string amount = reader.ReadSingle().ToString("0.00");
+
+                    offset += 4;
+                    reader.BaseStream.Seek(offset, SeekOrigin.Begin);
+                    string vol = reader.ReadUInt32().ToString();
+
+                    offset += 4;
+                    reader.BaseStream.Seek(offset, SeekOrigin.Begin);
+                    string reservation = reader.ReadUInt32().ToString();
+
+                    string msg = string.Format("{0} : {1} : {2} : {3} : {4} : {5} : {6} : {7} ", date, open, high, low, close, amount, vol, reservation);
+                    Console.WriteLine(msg);
+                }
+            }
+
+            Console.ReadKey(false);
+        }
+
+        // 读取板块数据
+        public static void Main2(string[] args)
+        {
+            // block_gn.dat
+            // block_zs.dat
+            // block_fg.dat
+            using (var reader = new BinaryReader(File.OpenRead(@"r:\block_gn.dat")))
+            {
+                // 文件信息
+                string fileInfoStr = Encoding.Default.GetString(reader.ReadBytes(64)).TrimEnd('\0');
+                Console.WriteLine("文件信息：{0}", fileInfoStr);
+
+                int indexStart = reader.ReadInt32(); // 板块索引信息起始位置
+                int bkInfoStart = reader.ReadInt32(); // 板块记录信息起始位置
+                Console.WriteLine("板块索引信息起始位置：{0}", indexStart);
+                Console.WriteLine("板块记录信息起始位置：{0}", bkInfoStart);
+
+                reader.BaseStream.Seek(indexStart, SeekOrigin.Begin);
+                // 索引名称
+                string indexName = Encoding.Default.GetString(reader.ReadBytes(64)).TrimEnd('\0');
+                Console.WriteLine("索引名称：{0}", indexName);
+
+                reader.BaseStream.Seek(bkInfoStart, SeekOrigin.Begin);
+                // 板块数量
+                int bkCount = reader.ReadInt16();
+                Console.WriteLine("板块数量：{0}", bkCount);
+
+                // 第一个版块的起始位置为0x182h。
+                int offect = bkInfoStart + 2;
+                for (int i = 0; i < bkCount; i++)
+                {
+                    reader.BaseStream.Seek(offect, SeekOrigin.Begin);
+                    // 板块名称
+                    string bkName = Encoding.Default.GetString(reader.ReadBytes(9)).TrimEnd('\0');
+                    // 证券数量
+                    int stockCount = reader.ReadInt16();
+                    // 板块级别 
+                    int bkLevel = reader.ReadInt16();
+
+                    Console.WriteLine("板块名称：{0}     证券数量：{1}    板块级别：{2}", bkName, stockCount, bkLevel);
+
+                    // 每个板块最多包括400只股票。(2813 -9 - 2 - 2) / 7 =  400
+                    StringBuilder sb = new StringBuilder();
+                    for (int j = 0; j < 400; j++)
+                    {
+                        string stockCode = Encoding.Default.GetString(reader.ReadBytes(7)).TrimEnd('\0');
+                        if (stockCode.Length == 0)
+                            break;
+
+                        sb.Append(stockCode + ", ");
+                    }
+                    Console.WriteLine(sb.ToString());
+
+                    Console.WriteLine(Environment.NewLine);
+
+                    offect += 2813; // 每个板块占的长度为2813个字节。
+                }
+            }
+
+            Console.ReadKey(false);
+        }
+    }
+}
