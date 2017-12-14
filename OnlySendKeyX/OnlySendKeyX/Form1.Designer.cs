c257906277d3a73a57ef98f9fb7b1d142deb5901
chanfengsr <chanfengsr@gmail.com>
2017-12-14 11:35:40 +0800

    2017-12-14

---
+++ OnlySendKeyX/OnlySendKeyX/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace OnlySendKeyX
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnTest = new System.Windows.Forms.Button();
+            this.txtHandle = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(39, 58);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "开始";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Enabled = false;
+            this.btnStop.Location = new System.Drawing.Point(135, 58);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 0;
+            this.btnStop.Text = "结束";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnTest
+            // 
+            this.btnTest.Location = new System.Drawing.Point(171, 116);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(75, 23);
+            this.btnTest.TabIndex = 1;
+            this.btnTest.Text = "Test";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Visible = false;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+            // 
+            // txtHandle
+            // 
+            this.txtHandle.Location = new System.Drawing.Point(84, 19);
+            this.txtHandle.Name = "txtHandle";
+            this.txtHandle.Size = new System.Drawing.Size(100, 21);
+            this.txtHandle.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Handle";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(248, 141);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtHandle);
+            this.Controls.Add(this.btnTest);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Only Send Key X";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.TextBox txtHandle;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ OnlySendKeyX/OnlySendKeyX/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace OnlySendKeyX
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll", EntryPoint = "keybd_event", SetLastError = true)]
+        public static extern void keybd_event(Keys bVk, byte bScan, uint dwFlags, uint dwExtraInfo);
+
+        [DllImport("User32.dll", EntryPoint = "SendMessage")]
+        private static extern IntPtr SendMessage(int hWnd, int msg, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("User32.dll", EntryPoint = "FindWindow")]
+        private static extern int FindWindow(string lpClassName, string lpWindowName);
+
+        //定义消息常数 
+        public const int CUSTOM_MESSAGE = 0X400 + 2;//自定义消息
+
+        [DllImport("user32.dll", EntryPoint = "PostMessageA", SetLastError = true)]
+        public static extern int PostMessage(IntPtr hWnd, int Msg, Keys wParam, int lParam);
+        public const int WM_CHAR = 256;
+        public const int WM_KEYDOWN = 256;
+        public const int WM_KEYUP = 257;
+
+        private IntPtr _windowHandler=IntPtr.Zero;
+
+        //向窗体发送消息的函数 
+        public void SendMsgToMainForm(int msg)
+        {
+            int WINDOW_HANDLER = FindWindow(null, "无标题 - 记事本");
+            if (WINDOW_HANDLER == 0)
+            {
+                throw new Exception("Could not find Main window!");
+            }
+
+            //long result = SendMessage(WINDOW_HANDLER, CUSTOM_MESSAGE, new IntPtr(14), IntPtr.Zero).ToInt64();
+            SendMessage(WINDOW_HANDLER, 88, new IntPtr(14), IntPtr.Zero);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //SendKeys.Send("X");
+            //keybd_event(Keys.X, 0, 0, 0);
+
+            if (_windowHandler == IntPtr.Zero)
+                SendKeys.Send("X");
+            else
+                PostMessage(_windowHandler, WM_CHAR, Keys.X, 1);
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            int intPtr;
+            if (int.TryParse(txtHandle.Text, System.Globalization.NumberStyles.AllowHexSpecifier, CultureInfo.InvariantCulture, out intPtr))
+                _windowHandler = (IntPtr)intPtr;
+            else
+                _windowHandler = IntPtr.Zero;
+
+            timer1.Enabled = true;
+
+            btnStart.Enabled = false;
+            btnStop.Enabled = true;
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+
+            btnStart.Enabled = true;
+            btnStop.Enabled = false;
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            IntPtr windowHandler = (IntPtr)FindWindow(null, "无标题 - 记事本");
+            int intPtr = int.Parse("00090B18", System.Globalization.NumberStyles.AllowHexSpecifier);
+
+            windowHandler = (IntPtr)0x00090B18;
+            //txtHandle.Focus();
+            //windowHandler = txtHandle.Handle;
+            PostMessage(windowHandler, WM_CHAR, Keys.A, 1);
+            PostMessage(windowHandler, WM_CHAR, Keys.A, 2);
+        }
+    }
+}
