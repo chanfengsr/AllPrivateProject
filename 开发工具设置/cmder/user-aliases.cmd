160d682d494c13c19c3cc08ffc71c7501a76879f
chanfengsr@gmail.com <chanfengsr@gmail.com>
2018-11-11 00:31:32 +0800

    cmder/user-aliases.cmd

--- 开发工具设置/cmder/user-aliases.cmd
+++ 开发工具设置/cmder/user-aliases.cmd
@@ -16,32 +16,33 @@ cmderr=cd /d "%CMDER_ROOT%"
 
 pwd=echo VPS:fdhg9901314 & echo BIN:K4BWB2+R & echo Sonepar:P@ssword01 & echo SAG:Chanfengsr026!
 edit=EditPlus /n
-bcomp=BCompare
+bcomp="%Software%\ExtractProgram\Beyond Compare\BCompare.exe"
 vps=ssh root@45.77.22.199
 inet=inetcpl.cpl
 ncpa=ncpa.cpl
+sysdm=sysdm.cpl
 
 bcomp0=echo 1 > 1.txt & echo 2 > 2.txt
-bcomp1=BCompare 1.txt 2.txt
+bcomp1="%Software%\ExtractProgram\Beyond Compare\BCompare.exe" 1.txt 2.txt
 config=EditPlus "%CMDER_ROOT%\config\user-aliases.cmd"
 wx="C:\Program Files (x86)\Tencent\WeChat\WeChat.exe"
 qq="C:\Program Files (x86)\Tencent\TIM\Bin\QQScLauncher.exe"
 music="C:\Program Files (x86)\Netease\CloudMusic\cloudmusic.exe"
-miaosu="D:\Software\ExtractProgram\MiaoSSÃîËÙ\MiaoSSÃîËÙ.exe"
 skype="C:\Program Files (x86)\Microsoft\Skype for Desktop\Skype.exe"
 ie="C:\Program Files\Internet Explorer\iexplore.exe"
 repOutlook="C:\Program Files\Microsoft Office\Office16\OUTLOOK.EXE /safe"
 gitExt="C:\Program Files (x86)\GitExtensions\GitExtensions.exe"
+miaosu="%Software%\ExtractProgram\MiaoSSå¦™é€Ÿ\MiaoSSå¦™é€Ÿ.exe"
 
-proj=explorer D:\Project
-allGit=explorer D:\Project\Private\AllInGitHub
-son=explorer D:\Project\AX\Sonepar
-bin=explorer D:\Project\AX\BIN
-quant=explorer D:\Project\AX\QUANT
-maz=explorer D:\Project\AX\Mazak
-hoya=explorer D:\Project\AX\HOYA
-dan=explorer D:\Project\AX\DAN
+proj=explorer %Project%
+allGit=explorer %Project%\AllInGitHub
+son=explorer %Project%\AX\Sonepar
+bin=explorer %Project%\AX\BIN
+quant=explorer %Project%\AX\QUANT
+maz=explorer %Project%\AX\Mazak
+hoya=explorer %Project%\AX\HOYA
+dan=explorer %Project%\AX\DAN
 
 .build=dotnet build --runtime win-x64
-pGeekTime=cd /d "D:\Project\Private\AllInGitHub\DoNetCore\geekTime"
-gt1="D:\Project\Private\AllInGitHub\DoNetCore\geekTime\bin\Debug\netcoreapp2.1\win-x64\geekTime.exe"
+pGeekTime=cd /d "%Project%\AllInGitHub\DoNetCore\geekTime"
+gt1="%Project%\AllInGitHub\DoNetCore\geekTime\bin\Debug\netcoreapp2.1\win-x64\geekTime.exe"
