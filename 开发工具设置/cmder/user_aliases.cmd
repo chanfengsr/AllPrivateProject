eba648fd4190523f0772da5f1f5439f3ba2c7dce
CFSR <chanfengsr@gmail.com>
2018-11-23 10:05:10 +0800

    cmder

--- 开发工具设置/cmder/user_aliases.cmd
+++ 开发工具设置/cmder/user_aliases.cmd
@@ -4,6 +4,23 @@
 ;= rem In batch mode, jump to the end of the file
 ;= goto:eof
 ;= Add aliases below here
+
+;= VSCode 打开
+;= CRLF, 编码 gb2312
+
+;=Path 环境变量
+;=C:\Program Files\wkhtmltopdf\bin
+;=D:\Software\ExtractProgram\ffmpeg\bin
+;=D:\Software\ExtractProgram\EditPlus3\x64
+;=D:\Software\ExtractProgram\Beyond Compare
+
+;=新建环境变量
+;=D:\Project
+;=D:\Software
+
+
+
+
 e.=explorer .
 gl=git log --oneline --all --graph --decorate  $*
 ls=ls --show-control-chars -F --color $*
@@ -15,6 +32,7 @@ cmderr=cd /d "%CMDER_ROOT%"
 
 initPath=echo cmd /k "%ConEmuDir%\..\init.bat"  -new_console:d:R:\
 pwd=echo VPS:fdhg9901314 & echo BIN:K4BWB2+R & echo Sonepar:P@ssword01 & echo SAG:Chanfengsr026! & echo WZYHome:1qaz@WSX
+dns=echo 210.22.70.3 & echo 210.22.84.3
 edit=EditPlus /n
 bcomp="%Software%\ExtractProgram\Beyond Compare\BCompare.exe"
 vps=ssh root@45.77.22.199
@@ -26,14 +44,14 @@ bcomp0=echo 1 > 1.txt & echo 2 > 2.txt
 bcomp1="%Software%\ExtractProgram\Beyond Compare\BCompare.exe" 1.txt 2.txt
 config=EditPlus "%CMDER_ROOT%\config\user_aliases.cmd"
 wx="C:\Program Files (x86)\Tencent\WeChat\WeChat.exe"
-qq="C:\Program Files (x86)\Tencent\TIM\Bin\QQScLauncher.exe"
+qq="D:\Program Files (x86)\Tencent\TIM\Bin\QQScLauncher.exe"
 music="C:\Program Files (x86)\Netease\CloudMusic\cloudmusic.exe"
 skype="C:\Program Files (x86)\Microsoft\Skype for Desktop\Skype.exe"
 ie="C:\Program Files\Internet Explorer\iexplore.exe"
 repOutlook="C:\Program Files\Microsoft Office\Office16\OUTLOOK.EXE /safe"
 gitExt="C:\Program Files (x86)\GitExtensions\GitExtensions.exe"
 miaosu="%Software%\ExtractProgram\MiaoSS妙速\MiaoSS妙速.exe"
-pyc="C:\Program Files\JetBrains\PyCharm Community Edition 183.3283.4\bin\pycharm64.exe"
+pyc=pycharm64.exe
 
 proj=explorer %Project%
 allGit=explorer %Project%\AllInGitHub
