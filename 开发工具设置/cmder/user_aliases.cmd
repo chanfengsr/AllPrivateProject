7af5c3f6049502d96ac3246faa55946a66244db7
CFSR <chanfengsr@gmail.com>
2019-01-21 11:57:16 +0800

    user_aliases.cmd

--- 开发工具设置/cmder/user_aliases.cmd
+++ 开发工具设置/cmder/user_aliases.cmd
@@ -31,7 +31,7 @@ vi          = vim $*
 cmderr      = cd /d "%CMDER_ROOT%"
 
 initPath    = echo cmd /k "%ConEmuDir%\..\init.bat"  -new_console:d:R:\
-pwd         = echo VPS:fdhg9901314 & echo BIN:K4BWB2+R & echo Sonepar:P@ssword01 & echo SAG:Chanfengsr026! & echo WZYHome:1qaz@WSX
+pwd         = echo VPS:fdhg9901314 & echo BIN:K4BWB2+R & echo Sonepar:P@ssword01 & echo SAG:Chanfengsr026! & echo VPN:wangy/KC02!L & echo WZYHome:1qaz@WSX
 dns         = echo 210.22.70.3 & echo 210.22.84.3
 edit        = EditPlus /n
 bcomp       = "%Software%\ExtractProgram\Beyond Compare\BCompare.exe"
@@ -45,12 +45,13 @@ bcomp1      = "%Software%\ExtractProgram\Beyond Compare\BCompare.exe" 1.txt 2.tx
 config      = EditPlus "%CMDER_ROOT%\config\user_aliases.cmd"
 wx          = "C:\Program Files (x86)\Tencent\WeChat\WeChat.exe"
 qq          = "D:\Program Files (x86)\Tencent\TIM\Bin\QQScLauncher.exe"
+wxqq        = "C:\Program Files (x86)\Tencent\WeChat\WeChat.exe" & "D:\Program Files (x86)\Tencent\TIM\Bin\QQScLauncher.exe"
 music       = "C:\Program Files (x86)\Netease\CloudMusic\cloudmusic.exe"
 skype       = "C:\Program Files (x86)\Microsoft\Skype for Desktop\Skype.exe"
 ie          = "C:\Program Files\Internet Explorer\iexplore.exe"
 repOutlook  = "C:\Program Files\Microsoft Office\Office16\OUTLOOK.EXE /safe"
 gitExt      = "C:\Program Files (x86)\GitExtensions\GitExtensions.exe"
-miaosu      = "%Software%\ExtractProgram\MiaoSS妙速\MiaoSS妙速.exe" & choice /t 10 /d y /n > nul & %Project%\AllInGitHub\Bat\ClearProxy.bat
+miaosu      = "%Software%\ExtractProgram\MiaoSS妙速\MiaoSS妙速.exe" & choice /t 15 /d y /n > nul & %Project%\AllInGitHub\Bat\ClearProxy.bat
 dict        = "%AppData%\..\Local\youdao\dict\Application\YoudaoDict.exe"
 vs          = "C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE\devenv.exe"
 pyc         = pycharm64.exe
@@ -80,7 +81,10 @@ quant       = explorer %WorkProject%\AX\QUANT
 maz         = explorer %WorkProject%\AX\Mazak
 hoya        = explorer %WorkProject%\AX\HOYA
 dan         = explorer %WorkProject%\AX\DAN
-kit        = explorer %WorkProject%\AX\大连京瓷
+kit         = explorer %WorkProject%\AX\大连京瓷
+kitDoc      = explorer %WorkProject%\AX\大连京瓷\Document\Own
+
+sql         = "D:\Program Files (x86)\Microsoft SQL Server\140\Tools\Binn\ManagementStudio\Ssms.exe"
 
 rm30        = mstsc /v:192.168.1.30
 rm70        = mstsc /v:192.168.1.70
