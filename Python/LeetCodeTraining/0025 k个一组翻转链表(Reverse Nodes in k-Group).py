931d7f5565fbb17650a2d0fae6f5a919f52e8506
CFSR <chanfengsr@gmail.com>
2019-02-26 23:43:03 +0800

    leetCode

--- Python/LeetCodeTraining/0025 k个一组翻转链表(Reverse Nodes in k-Group).py
+++ Python/LeetCodeTraining/0025 k个一组翻转链表(Reverse Nodes in k-Group).py
@@ -13,6 +13,7 @@ k 是一个正整数，它的值小于或等于链表的长度。如果节点总
 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
 '''
 
+
 class Solution:
     def reverseKGroup(self, head, k):
         """
@@ -20,7 +21,47 @@ class Solution:
         :type k: int
         :rtype: ListNode
         """
-        return None
+        if head is None:
+            return head
+
+        headNew = None  # 新的返回值
+        tail = None  # 单次反转的尾
+        pre = None
+        cur = head
+        while cur is not None:
+            canRev = True  # 当前位置往后，是否可以反转 k - 1 次
+
+            # 测试后面是否可以反转 k - 1 次
+            t = cur
+            i = k - 1  # 当前元素开始需要单次翻转次数
+            while i > 0:
+                t = t.next
+                if t is None:
+                    canRev = False
+                    break
+                i -= 1
+
+            # 开始反转
+            if canRev:
+                pre = None
+                tt = cur
+
+                i = k  # 反转次数
+                while i > 0:
+                    cur.next, pre, cur = pre, cur, cur.next
+                    i -= 1
+
+                if tail is not None:
+                    tail.next = pre
+                tail = tt
+                if headNew is None:
+                    headNew = pre
+            else:
+                if tail is not None:
+                    tail.next = cur
+                break
+
+        return headNew if headNew is not None else head
 
 
 # Definition for singly-linked list.
@@ -50,6 +91,10 @@ class ListNode:
 
 if __name__ == '__main__':
     s = Solution()
-    head = ListNode.creatByList(list(range(1, 6)))
-    ret = s.reverseKGroup(head, 3)
-    if ret:ret.print()
+    head = ListNode.creatByList(list(range(1, 5 + 1)))
+
+    head = None
+
+    ret = s.reverseKGroup(head, 6)
+    if ret:
+        ret.print()
