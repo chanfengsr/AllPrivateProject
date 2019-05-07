9f58d3f3494e0942ed00fb79f64d7d38fa906418
CFSR <chanfengsr@gmail.com>
2019-05-07 12:59:52 +0800

    megerVA

--- DoNetCore/megerVA/Program.cs
+++ DoNetCore/megerVA/Program.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
@@ -12,7 +12,7 @@ namespace megerVA
         static void Main(string[] args)
         {
             string workDir = @"R:\"; // System.Environment.CurrentDirectory;
-            
+
             // 有参数传入路径的话就用传入的
             if (args != null && args.Length > 0)
                 if (System.IO.Directory.Exists(args[0]))
@@ -22,21 +22,53 @@ namespace megerVA
             Console.WriteLine(string.Format("Work directory: {0}", dirInfo.FullName));
 
             List<string> lstFileFullName = new List<string>();
-            foreach (FileInfo file in dirInfo.GetFiles().Where(f => f.Extension.ToLower() == ".mp4") )
+            foreach (FileInfo file in dirInfo.GetFiles().Where(f => f.Extension.ToLower() == ".mp4"))
                 lstFileFullName.Add(file.FullName);
 
             foreach (string fName in lstFileFullName)
             {
                 if (fName.EndsWith("1.mp4"))
-                {                    
-                    string audioName = fName.TrimEnd("1.mp4 ".ToCharArray()) + "2.mp4";
+                {
+                    string actName = fName.TrimEnd("1.mp4 ".ToCharArray());
+                    string audioName = actName + "2.mp4";
                     if (lstFileFullName.Contains(audioName))
                     {
-                        string tarName = fName.TrimEnd("1.mp4 ".ToCharArray()) + ".mp4";
-                        Console.WriteLine(string.Format(cmdMod,fName,audioName,tarName));                        
+                        if (actName == workDir)
+                            actName += "out";
+                        string tarName = actName + ".mp4";
+
+                        Console.WriteLine(string.Format(cmdMod, fName, audioName, tarName));
                     }
                 }
             }
         }
+
+        #region 调用DOS执行CMD方法
+
+        /// <summary>调用DOS执行CMD方法
+        /// </summary>
+        /// <param name="command">参数</param>
+        /// <returns></returns>
+        public static string RunCmd(string command = "")
+        {
+            //实例一个Process类，启动一个独立进程  
+            Process p = new Process();
+            //Process类有一个StartInfo属性，这个是ProcessStartInfo类，包括了一些属性和方法，下面我们用到了他的几个属性：  
+
+            p.StartInfo.FileName = "cmd.exe"; //设定程序名  
+            p.StartInfo.Arguments = "/c " + command; //设定程式执行参数  
+            p.StartInfo.UseShellExecute = false; //关闭Shell的使用  
+            p.StartInfo.RedirectStandardInput = true; //重定向标准输入  
+            p.StartInfo.RedirectStandardOutput = true; //重定向标准输出  
+            p.StartInfo.RedirectStandardError = true; //重定向错误输出  
+            p.StartInfo.CreateNoWindow = true; //设置不显示窗口  
+
+            p.Start(); //启动  
+            p.StandardInput.WriteLine("exit"); //不过要记得加上Exit要不然下一行程式执行的时候会当机  
+
+            return p.StandardOutput.ReadToEnd(); //从输出流取得命令执行结果  
+
+        }
+        #endregion 调用DOS执行CMD方法
     }
 }
